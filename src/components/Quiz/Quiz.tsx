afeb615af1b7456ab13782ef1d6d2fcb9dfddaed
hiroki takada <hirorotaka5@gmail.com>
2025-02-17 09:58:46 +0900

    Quizのカスタムフック化

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -1,11 +1,11 @@
-import { ChangeEvent, Dispatch, SetStateAction, useRef, useState } from 'react';
-import { Question, ResultState } from '../../types';
-import { resultInitialState } from '../../quizConstants';
+import { Dispatch, SetStateAction } from 'react';
+import { Question } from '../../types';
 import { QuizResult } from '../QuizResult/QuizResult';
 import { AnswerTimer } from '../AnswerTimer/AnswerTimer';
 import { AnswerEffect } from '../AnswerTimer/AnswerEffect';
 import InputAnswer from './InputAnswer';
 import ChoiceAnswer from './ChoiceAnswer';
+import { useQuiz } from '../../hooks/useQuiz';
 
 type QuizProps = {
   questions: Question[];
@@ -14,102 +14,25 @@ type QuizProps = {
 };
 
 export const Quiz = ({ questions, playerName, setQuizStarted }: QuizProps) => {
-  // 問題・回答関連
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
-  const [answerIdx, setAnswerIdx] = useState<number | null>(null);
-  const [answer, setAnswer] = useState<boolean | null>(null);
-  const [inputAnswer, setInputAnswer] = useState('');
-
-  // UI表示制御
-  const [showAnswerTimer, setShowAnswerTimer] = useState(true);
-  const [showEffect, setShowEffect] = useState(false);
-  const [isCorrect, setIsCorrect] = useState(false);
-  const [showResult, setShowResult] = useState(false);
-
-  // 結果管理
-  const [result, setResult] = useState<ResultState>(resultInitialState);
-
-  // 処理制御用ref
-  const isProcessingAnswer = useRef(false);
-
-  // 現在の問題
-  const { question, choices, type, correctAnswer } =
-    questions[currentQuestionIndex];
-
-  // 選択式問題の回答処理
-  const onAnwserClick = (answer: string, index: number) => {
-    setAnswerIdx(index);
-    if (answer === correctAnswer) {
-      setAnswer(true);
-    } else {
-      setAnswer(false);
-    }
-  };
-
-  // テキスト入力問題の回答処理
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputAnswer(e.target.value);
-    setAnswer(e.target.value === correctAnswer);
-  };
-
-  const processAnswer = (finalAnswer: boolean | null) => {
-    // 重複回答防止
-    if (isProcessingAnswer.current) return;
-    isProcessingAnswer.current = true;
-
-    setShowEffect(true); // 正誤エフェクト表示
-    setInputAnswer('');
-    setIsCorrect(finalAnswer === true);
-
-    // 300ms後に次の問題へ移行
-    setTimeout(() => {
-      setShowEffect(false);
-      setAnswerIdx(null);
-      setShowAnswerTimer(false);
-      // スコアと正誤カウントの更新
-      setResult((prev) =>
-        finalAnswer
-          ? {
-              ...prev,
-              score: prev.score + 10,
-              correctAnswers: prev.correctAnswers + 1,
-            }
-          : {
-              ...prev,
-              wrongAnswers: prev.wrongAnswers + 1,
-            }
-      );
-
-      // 次の問題への移行か結果表示の判定
-      if (currentQuestionIndex !== questions.length - 1) {
-        setCurrentQuestionIndex((prev) => prev + 1);
-      } else {
-        setCurrentQuestionIndex(0);
-        setAnswerIdx(null);
-        setShowResult(true);
-      }
-
-      setTimeout(() => {
-        setShowAnswerTimer(true);
-        isProcessingAnswer.current = false;
-      }, 0);
-    }, 300);
-  };
-
-  const onClickNext = () => {
-    processAnswer(answer);
-  };
-
-  const handleTimeUp = () => {
-    processAnswer(false);
-  };
-
-  const onTryAgain = () => {
-    setResult(resultInitialState);
-    setShowResult(false);
-    setQuizStarted(false);
-  };
-
+  const {
+    currentQuestion,
+    answerIdx,
+    inputAnswer,
+    isCorrect,
+    showAnswerTimer,
+    showEffect,
+    showResult,
+    result,
+    currentQuestionIndex,
+    onClickNext,
+    onAnwserClick,
+    handleInputChange,
+    handleTimeUp,
+    onTryAgain,
+  } = useQuiz({
+    questions,
+    setQuizStarted,
+  });
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 px-4 sm:px-6 lg:px-8">
       {/* クイズ結果 */}
@@ -133,10 +56,10 @@ export const Quiz = ({ questions, playerName, setQuizStarted }: QuizProps) => {
                 <span className="text-gray-500">/{questions.length}</span>
               </div>
               <h2 className="mb-6 break-words text-2xl font-semibold">
-                {question}
+                {currentQuestion.question}
               </h2>
               <div className="min-h-[200px]">
-                {type === 'textInput' ? (
+                {currentQuestion.type === 'textInput' ? (
                   <InputAnswer
                     value={inputAnswer}
                     onInputChange={handleInputChange}
@@ -144,7 +67,7 @@ export const Quiz = ({ questions, playerName, setQuizStarted }: QuizProps) => {
                 ) : (
                   <ChoiceAnswer
                     answerIdx={answerIdx}
-                    choices={choices}
+                    choices={currentQuestion.choices}
                     onAnswerClick={onAnwserClick}
                   />
                 )}
